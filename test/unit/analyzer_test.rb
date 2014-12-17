2a29263c3fc86c3f5fa4671eb079b612b01e487e
geordie <george.speake@gmail.com>
2014-12-17 13:40:42 -0800

    change analyzer to have state keep track of the sets internally.

--- test/unit/analyzer_test.rb
+++ test/unit/analyzer_test.rb
@@ -6,7 +6,7 @@ class AnalyzerTest < DomGlancyTestCase
     master_data = array_of_elements
     current_data = array_of_elements
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 0, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 0, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -18,7 +18,7 @@ class AnalyzerTest < DomGlancyTestCase
     current_data = array_of_elements_small
     current_data.first['id'] = 'some-new-id'
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 1, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 1, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -30,7 +30,7 @@ class AnalyzerTest < DomGlancyTestCase
     current_data = array_of_elements
     current_data << single_element_hash
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 1, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 0, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -43,7 +43,7 @@ class AnalyzerTest < DomGlancyTestCase
     current_data[0]['height'] = current_data[0]['height'] + 16
     current_data[1]['width'] = current_data[1]['width'] + 25
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 0, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 0, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -58,7 +58,7 @@ class AnalyzerTest < DomGlancyTestCase
 
     current_data[1]['height'] = current_data[1]['height'] + 3
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 0, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 0, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -75,7 +75,7 @@ class AnalyzerTest < DomGlancyTestCase
     current_data.delete_at(5)                                    ## the one missing
     current_data << single_element_hash                          ## the one added
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     assert_equal 1, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 1, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
@@ -87,7 +87,7 @@ class AnalyzerTest < DomGlancyTestCase
     master_data  = travis_local_generated_master
     current_data = travis_generated_current
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
     assert_equal 0, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
     assert_equal 0, analysis[:not_in_current].count, 'results of data analysis: not_in_current'
     assert_equal 0, analysis[:changed_element_pairs].count, 'changed element pairs'
@@ -98,7 +98,7 @@ class AnalyzerTest < DomGlancyTestCase
     master_data  = travis_local_generated_master_2
     current_data = travis_generated_current_2
 
-    analysis = DomGlancy::Analyzer.new.analyze(master_data, current_data)
+    analysis = DomGlancy::Analyzer.new(master_data, current_data).analyze
 
     ## real difference that was found on travis with the INPUT tags being different styles/sizes if not css-styled
     assert_equal 0, analysis[:not_in_master].count, 'results of data analysis: not_in_master'
