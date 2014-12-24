1c33c7caa95ba303078b76e7a0cb2eefc9e0f1ce
geordie <george.speake@gmail.com>
2014-12-23 16:43:42 -0800

    move show page js to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,18 @@ function toggleDetailSection(section_id, event) {
     }
 }
 
+function highlightElement (element_id) {
+    var elements = document.getElementsByTagName('rect');
+    var element = null;
+    var length = elements.length;
+
+    for (var i = 0; i < length; i++) {
+        elements[i].setAttribute("stroke-width", "1");
+    }
+    var this_element = document.getElementById(element_id);
+    this_element.setAttribute("stroke-width", "5");
+}
+
 document.getElementById("changed_toggle").addEventListener("click", function(e) { toggleDetailSection('js--changed', e) });
 document.getElementById("not_master_toggle").addEventListener("click", function(e) { toggleDetailSection('js--not_master', e) });
 document.getElementById("not_current_toggle").addEventListener("click", function(e) { toggleDetailSection('js--not_current', e) });
